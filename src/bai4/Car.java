27045bcb2366b605c59593f34ab916eb6d47370e
Huyết tử Cool <baokyeu12345@gmail.com>
2025-05-14 08:50:32 +0700

    update car running

--- src/bai4/Car.java
+++ src/bai4/Car.java
@@ -1,13 +1,18 @@
 package bai4;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle {
 
     @Override
     void abstractsion_move() {
         System.out.println("Car is moving");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Car running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-} 
-    
-
-
+}
