5dfcc60eb46c9fdd0af7b5fdf51581f2139be016
Vu Dinh Ba <Baokyeu12345@gmail.com>
2025-05-15 14:18:01 +0700

    update washing

--- src/bai4/Vehicle.java
+++ src/bai4/Vehicle.java
@@ -3,5 +3,13 @@ package bai4;
 public abstract class Vehicle {
 
     abstract void abstractsion_move();
+
+    void mive(){
+        System.out.println("New car buy is the Garage");
+
+    }
     
+    void washing(){
+        System.out.println("Washing car");
+    }
 }
