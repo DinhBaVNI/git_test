cda169340e9bdc70f80ddc14ebbd26b17e6c026a
Vu Dinh Ba <Baokyeu12345@gmail.com>
2025-05-17 08:49:43 +0700

    Update mive()

--- src/bai4/Vehicle.java
+++ src/bai4/Vehicle.java
@@ -4,12 +4,12 @@ public abstract class Vehicle {
 
     abstract void abstractsion_move();
 
-    void mive(){
-        System.out.println("New car buy is the Garage");
+    void mive() {
+        System.out.println("Xe may bi chay");
 
-    }  
-    
-    void washing(){
+    }
+
+    void washing() {
         System.out.println("Washing car");
     }
 }
